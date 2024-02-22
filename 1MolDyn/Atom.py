fixed units added energies and plotted them :) milestone 2 reached we rock

1MolDyn/Atom.py
@@ -17,7 +17,12 @@ class Atom:
         self.pos = np.array(pos)
         self.oldpos = self.pos # update at the same time
         self.vel = np.array(vel)
-
+        
+        # Energies
+        self.kinetic = 0
+        self.potential = 0
+        self.total = 0
+        
         # Other bodies that it cares about, also class atom. Added with
         # get_friends() method
         self.friends = []
@@ -54,7 +59,15 @@ class Atom:
                 
         # NOTE: May be slow
         self.friends = rs[ rs > 0]
-                    
+    
+    def lj_pot(self, r):
+        ''' Calculates the Lennard-Jones potential '''
+        prefactor = 4 * c.epsilon
+        pauli = c.sigma**12 / r**12
+        vdWaals = c.sigma**6 / r**6
+        U = prefactor * ( pauli - vdWaals )
+        return U
+
     def lj_pot_prime(self, r):
         ''' Calculates the dU/dr for the Lennard-Jones potential '''
         prefactor = 4 * c.epsilon
@@ -82,17 +95,29 @@ class Atom:
                 self.pos[d] -= c.boxL * np.floor(self.pos[d] * c.inv_boxL)
         self.oldpos = self.pos
                 
-    def euler_step(self, particles):
+    def euler_step(self):
         ''' Naive Euler Step that does not conserve energy'''
-        self.get_friends(particles) # Apply the minimum imaging convention
         # self.am_i_in_the_box()
         self.oldpos = self.pos
-        self.pos += self.vel * c.h
-        self.vel += self.accleration() * c.h
+        self.pos += self.vel * c.h * c.t_tilde # time units!!
+        self.vel += self.accleration() * c.h * c.t_tilde
+
+    def energy_update(self, particles):
+        ''' Naive Euler Step that does not conserve energy'''
+
+        self.kinetic = 0.5 * c.EPSILON * (np.linalg.norm(self.vel) * \
+                                          c.vel_to_cgs)**2
+        potential = 0
+        for r in self.friends:
+            potential += self.lj_pot(r)
+        self.potential = potential
+        self.total = self.kinetic + potential
+
+    def update(self, particles):
+        self.get_friends(particles) # Apply the minimum imaging convention
+        self.euler_step()
         self.am_i_in_the_box()
-
-
-        
+        self.energy_update(particles)
     
     # def leapfrog_step(self, h = half_day):
     #     if self.friends != None:

1MolDyn/Particles.py
@@ -21,7 +21,8 @@ class Particles:
 
         self.all_positions  = [self.positions]
         self.all_velocities = [self.velocities]
-
+        self.all_energies = [self.energies]
+        
     @property
     def positions(self) -> np.ndarray:
         """
@@ -65,7 +66,26 @@ class Particles:
         """
         for i, particle in enumerate(self.particles):
             particle.vel = new_vels[i]
-
+            
+    @property
+    def energies(self) -> np.ndarray:
+        """
+        Get energies of all particles.
+        Returns an (3, number of particles) array,
+        with kinetic on row 0, potential on row 1, and the sum on row 2
+        """
+        energies = np.zeros((3, len(self.particles)))
+        for i, particle in enumerate(self.particles):
+            energies[:,i] = [particle.kinetic, particle.potential, particle.total]
+        return energies
+    
+    @energies.setter
+    def energies(self, new_ene : np.ndarray):
+        """
+        No such thing as an energy setter
+        """
+        raise ValueError('We never set energies explicitly; you vile, Noether-defying creature')
+            
     @property
     def colors(self) -> list:
         """
@@ -95,8 +115,10 @@ class Particles:
         taking the forces from the other particles into account.
         """
         for particle in self.particles:
-            particle.euler_step(self.particles)
+            particle.update(self.particles)
         
         # save positions and velocities to a big list containing past positions and velocities of all particles
         self.all_positions.append(self.positions)
         self.all_velocities.append(self.velocities)
+        self.all_energies.append(self.energies)
+        # self.all_energies.

1MolDyn/prelude.py
@@ -5,6 +5,7 @@ Created on Fri Feb  9 12:18:14 2024
 
 @authors: konstantinos & diederick
 """
+import numpy as np
 
 # Simulation parameters
 Nbodies = 9
@@ -12,21 +13,30 @@ h = 0.01
 boxL = 100 # Units??
 inv_boxL = 1 / boxL
 dims = 2 
-timesteps = 10000
+timesteps = 30_000
 plot_number = 1000
 
 rngseed = 8
 
-# Constants
-epsilon = 1 # 119.8 # [K]
-sigma   = 1 # 3.405 # [Angstrom]
-m_argon = 1 # 39.792 # [amu] 
-
+# N-body units
+epsilon = 1
+sigma   = 1 
+m_argon = 1
 
 # Derived
+EPSILON = 119.8 # [K]
+SIGMA = 3.405 # [Angstrom]
+M_ARGON = 39.792 # [amu] 
+K_BOLTZMANN = 1.3803658e-16 # [cgs]
 steps_per_plot = timesteps / plot_number 
 inv_m_argon = 1/m_argon
+t_tilde = 1 / np.sqrt((M_ARGON * SIGMA**2 / EPSILON ))
 
+# Converters | ALWAYS multiply by the converter
+amu_to_gram = 1.66054e-24
+angstrom_to_cm = 1e-8
+time_to_cgs = np.sqrt(amu_to_gram/K_BOLTZMANN) * angstrom_to_cm * t_tilde
+vel_to_cgs =  t_tilde / SIGMA
 
 #Pretty stuff
 # Plotting
@@ -46,7 +56,7 @@ colors = [c91, c92, c93, c94, c95, c96, c97, c98, c99]
 
 # Plotting
 import matplotlib.pyplot as plt
-plt.rcParams['text.usetex'] = True
+plt.rcParams['text.usetex'] = False # be FAST
 plt.rcParams['figure.dpi'] = 300
 plt.rcParams['font.family'] = 'Times New Roman'
 plt.rcParams['figure.figsize'] = [6 , 5]

1MolDyn/simulation.py
@@ -24,7 +24,7 @@ simnum = int(max([0]+[float(file[3:])for file in listdir('sims')])+1)
 makedirs(f"sims/sim{simnum}", exist_ok=True)
 
 def make_movie(simnum=simnum, name='moive'):
-    system(f'ffmpeg -i sims/sim{simnum}/fig%d.png -c:v libx264 -r 30 sims/sim{simnum}/{name}.mp4')
+    system(f'ffmpeg -i sims/sim{simnum}/fig%d.png -c:v libx264 -r 30 sims/sim{simnum}/{name}.mp4 -loglevel panic')
 
 
 plt.ioff()
@@ -32,18 +32,19 @@ def make_plot(index, particles):
     figno = int(index/c.steps_per_plot)
     fig, ax = plt.subplots(figsize = (6,5))
     
-    #get relevant info
+    # get relevant info
     pos, vels, colors = particles.all_positions[index], particles.all_velocities[index], particles.colors
-    #calculate the normalised velocity coordinates for arrows
+    
+    # calculate the normalised velocity coordinates for arrows
     vnorms = vels/np.linalg.norm(vels, axis=0)
     
-    #plot particles
+    # plot particles
     ax.scatter(pos[0], pos[1], c = colors, zorder=3)
     
-    #plot arrows
+    # plot arrows
     ax.quiver(pos[0], pos[1], vnorms[0], vnorms[1], angles='xy', zorder=2)
     
-    #plot the box
+    # plot the box
     ax.set_xlim(-0.1 * c.boxL, 1.1 * c.boxL), ax.set_ylim(-0.1 * c.boxL, 1.1 * c.boxL)
     ax.axhline(0, c = 'k', linestyle = '--'), ax.axhline(c.boxL, c = 'k', linestyle = '--')
     ax.axvline(0, c = 'k', linestyle = '--'), ax.axvline(c.boxL, c = 'k', linestyle = '--')
@@ -52,8 +53,8 @@ def make_plot(index, particles):
     plt.savefig(f'sims/sim{simnum}/fig{figno}')
     plt.close(fig)
 
-#Testing
-#Collision
+# Testing
+# Collision
 # particles = Particles([Atom(pos = [0.1*c.boxL, 0.5*c.boxL], vel=[0.5 , 0], color=c.colors[0]),
 #                        Atom(pos = [0.5*c.boxL, 0.5*c.boxL], vel=[-0.5, 0], color=c.colors[1])])
 
@@ -62,22 +63,33 @@ def make_plot(index, particles):
 particles = Particles(c.Nbodies, seed=c.rngseed)
 
 
-# Moved this to be inside the Particles class
-# Init random number generator
-# rng = np.random.default_rng(seed=c.rngseed)
-# particles = []
-# for i in range(c.Nbodies):
-#     pos = rng.random(c.dims)*c.boxL
-#     vel = rng.random(c.dims)*2 - 1 # -1 to 1
-#     temp = Atom(pos, vel, c.colors[i])
-#     particles.append(temp)
-
 # First calculate all positions/velocities
 for i in range(c.timesteps):
     particles.update()
+#%%
+def energy_plot():
+    time = np.arange(0, c.timesteps + 1) * c.h * c.time_to_cgs * 1e12
+    plt.ion()
+    energies = particles.all_energies
+    kinetic = [ np.sum(energy[0]) for energy in energies]
+    potential = [ np.abs(np.sum(energy[1])) for energy in energies]
+    total = [ np.sum(energy[2]) for energy in energies]
 
+    fig, ax = plt.subplots(figsize = (4,4))
+    ax.plot(time, kinetic, c = c.c93)
+    ax.plot(time, potential, c = c.c97)
+    ax.plot(time, total, c = 'k', linestyle = '-.')
+
+    # Make pretty
+    ax.set_yscale('log')
+    # ax.set_xscale('')
+    ax.set_ylim(1e-12, 1e2)
+    ax.set_xlim(0, 0.5)
+    ax.set_xlabel('Time [ps]')
+    ax.set_ylabel('Energy [sim units]')
+energy_plot()
 #Then make all the plots
-for i in np.arange(0, c.timesteps, c.steps_per_plot):
-    make_plot(int(i), particles)
+# for i in np.arange(0, c.timesteps, c.steps_per_plot):
+#     make_plot(int(i), particles)
 
-make_movie()
+# make_movie()