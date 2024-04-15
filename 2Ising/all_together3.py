7d39807a8012fe5d7caa62b9f00504a8b763bb8d
Diederick Vroom <diederick.vroom@gmail.com>
2024-04-15 23:36:52 +0200

    Checked stuff, small changes

--- 2Ising/all_together3.py
+++ 2Ising/all_together3.py
@@ -6,9 +6,9 @@ from plotters import grid as plot_grid
 from tqdm import tqdm
 import numba
 #%%
-kernel = np.array([[0, 1, 0], 
-                   [1, 0, 1], 
-                   [0, 1, 0]])
+kernel = 0.5 * np.array([[0, 1, 0], 
+                         [1, 0, 1], 
+                         [0, 1, 0]])
 
 rng  = np.random.default_rng(None)
 
@@ -19,7 +19,7 @@ def Hamiltonian(grid: np.ndarray[float]) -> float:
     return coupling_term + ext_mag_field_term
 
 @numba.njit( nopython=True, nogil=True)
-def metropolis(init_grid, steps, T, init_energy, H):
+def metropolis(init_grid, steps, T, init_energy, J, H):
     chunk_size = 20
     beta = 1/T
     N = len(init_grid)
@@ -60,39 +60,50 @@ def metropolis(init_grid, steps, T, init_energy, H):
         current_spin = last_grid[flip_row,flip_col]
         candidate_spin = current_spin * -1
 
-        # Periodic Boundary Conditions
-        if flip_row > 0 and flip_row < N:
-            top = last_grid[flip_row-1, flip_col]
-        elif flip_row == 0:
-            top = last_grid[-1, flip_col]
-        else: # flip_row == N
-            top = last_grid[N-1-1, flip_col] # indexing begins at 0
+        # # Periodic Boundary Conditions
+        # if flip_row > 0 and flip_row < N:
+        #     top = last_grid[flip_row-1, flip_col]
+        # elif flip_row == 0:
+        #     top = last_grid[-1, flip_col]
+        # else: # flip_row == N
+        #     top = last_grid[N-1-1, flip_col] # indexing begins at 0
             
-        if flip_row > 0 and flip_row < N:
-            bottom = last_grid[flip_row+1, flip_col]
-        elif flip_row == 0:
-            bottom = last_grid[1, flip_col]
-        else: # flip_row == N
-            bottom = last_grid[0, flip_col]
+        # if flip_row > 0 and flip_row < N:
+        #     bottom = last_grid[flip_row+1, flip_col]
+        # elif flip_row == 0:
+        #     bottom = last_grid[1, flip_col]
+        # else: # flip_row == N
+        #     bottom = last_grid[0, flip_col]
             
-        if flip_col > 0 and flip_col < N:
-            right = last_grid[flip_row, flip_col+1]
-        elif flip_col == 0:
-            right = last_grid[flip_row, 1]
-        else: # flip_col == N
-            right = last_grid[flip_row, 0]
+        # if flip_col > 0 and flip_col < N:
+        #     right = last_grid[flip_row, flip_col+1]
+        # elif flip_col == 0:
+        #     right = last_grid[flip_row, 1]
+        # else: # flip_col == N
+        #     right = last_grid[flip_row, 0]
         
-        if flip_col > 0 and flip_col < N:
-            left = last_grid[flip_row, flip_col-1]
-        elif flip_col == 0:
-            left = last_grid[flip_row, -1]
-        else: # flip_col == N
-            left = last_grid[flip_row, N-1-1]
+        # if flip_col > 0 and flip_col < N:
+        #     left = last_grid[flip_row, flip_col-1]
+        # elif flip_col == 0:
+        #     left = last_grid[flip_row, -1]
+        # else: # flip_col == N
+        #     left = last_grid[flip_row, N-1-1]
             
-        current_energy = -current_spin * (top + left + right + bottom)
-        candidate_energy = -candidate_spin * (top + left + right + bottom)
-        delta_E = candidate_energy - current_energy
+        # current_energy = -current_spin * (top + left + right + bottom)
+        # candidate_energy = -candidate_spin * (top + left + right + bottom)
+        # delta_E = candidate_energy - current_energy
         
+        top    = last_grid[(flip_row-1)%len(last_grid), flip_col]
+        left   = last_grid[flip_row, (flip_col-1)%len(last_grid)]
+        right  = last_grid[flip_row, (flip_col+1)%len(last_grid)]
+        bottom = last_grid[(flip_row+1)%len(last_grid), flip_col]
+
+
+        neighbors_sum = top + left + right + bottom
+        delta_E = 2*(J*neighbors_sum + H)*current_spin
+        
+        # delta_E = 2*current_spin * neighbors_sum
+
         # Accept or Deny ----------------------------------------------------->
         prob_ratio = np.exp(-delta_E * beta)
         if delta_E < 0 or np.random.random() < prob_ratio:
@@ -106,28 +117,32 @@ def metropolis(init_grid, steps, T, init_energy, H):
         all_avg_mags[i+1] = last_grid.mean()
     return all_energies, all_avg_mags, converged
 #%%
-N = 10
+N = 20
 MC_step = N**2
 steps = 10_000*MC_step
 J = 1
 H = 0
 chunk_size = 20
 x_array = np.arange(chunk_size*MC_step)
-Ts = np.arange(1, 4, step = 0.5)
+Ts = np.arange(1, 4, step = 0.1)
 mags = []
 convergance = []
 
-# 75% Positive
-flip = rng.integers(N, size = (2, N**2//4))
-init_grid = np.ones((N, N))
-init_grid[flip[0], flip[1]] = -1
+# ----- 75% Positive ----- #
+#flip = rng.integers(N, size = (2, N**2//4))
+# init_grid = np.ones((N, N))
+# init_grid[flip[0], flip[1]] = -1
+
+# ----- Random ----- #
+init_grid = np.sign(rng.random((N, N)) - 0.5)
+
 init_energy = Hamiltonian(init_grid)
 
 for T in tqdm(Ts):
-    e, m, c = metropolis(init_grid, steps, T, init_energy, H)
+    e, m, c = metropolis(init_grid, steps, T, init_energy, J, H)
     mags.append( m[-50*MC_step:].mean() )
     convergance.append(c)
-plt.plot(convergance)
+#plt.plot(convergance)
 # mags2 = []
 # init_grid = np.ones((N, N))*(-1)
 # init_grid[flip[0], flip[1]] = 1
@@ -138,13 +153,29 @@ plt.plot(convergance)
 #     mags2.append( m[-50*MC_step:].mean() )
 #%%    
 plt.plot(Ts, mags, 
-         c = 'k', marker = 'h', ls = ':', markersize = 10)
+         c = 'k', marker = 'h', ls = '', markersize = 10, zorder=3)
 # plt.plot(Ts, mags2, c = '#F1C410', 
 #           marker = 'h', ls = ':', markeredgecolor = 'k', markersize = 10)
-plt.axhline(0, c = 'maroon', ls ='--')
-plt.axvline(2.269, c = 'maroon', ls ='--')
+plt.axhline(0, c = 'maroon', ls ='--', zorder=2)
+plt.axvline(2.269, c = 'maroon', ls ='--', zorder=2)
 plt.xlabel('T', fontsize = 14)
 plt.ylabel('Avg mag', fontsize = 14)
 plt.title(f'2D ising model for N:{N}', fontsize = 15)
 plt.ylim(-1.2,1.2)
+plt.xlim(Ts.min()*0.8, Ts.max()*1.2)
 
+#%%
+for i in (ls := range(4)):
+    for j in range(len(ls)):
+        top    = ((i-1)%len(ls), j)
+        left   = (i, (j-1)%len(ls))
+        right  = (i, (j+1)%len(ls))
+        bottom = ((i+1)%len(ls), j)
+
+        print(f"       | {top} |        ")
+        print( "-------+--------+-------")
+        print(f"{left} | {i,j} | {right}")
+        print( "-------+--------+-------")
+        print(f"       | {bottom} |        ")
+        print()
+# %%
