848f526f5e454aa52867c78781c85cc3a46facb8
KKilmetis8 <asansol2552@gmail.com>
2024-05-17 22:54:28 +0200

    pp chain works in a hacky way

--- 3NRN/ppchain copy.py
+++ 3NRN/ppchain copy.py
@@ -8,12 +8,19 @@ Created on Wed May  1 16:27:11 2024
 
 import numpy as np
 import matplotlib.pyplot as plt
+plt.rcParams['text.usetex'] = False # be FAST
+plt.rcParams['figure.dpi'] = 300
+plt.rcParams['font.family'] = 'Times New Roman'
+plt.rcParams['figure.figsize'] = [4 , 4]
+plt.rcParams['axes.facecolor']= 'whitesmoke'
+plt.rcParams['xtick.direction'] = 'in'
+plt.rcParams['ytick.direction'] = 'in'
 from scipy.optimize import root
 import numba
 from tqdm import tqdm
 
 @numba.njit
-def eq(Ys, rates, old, h):
+def eq(Ys, old, rates, h):
     res = np.zeros(len(Ys))
     res[0] = -2*rates[0]*Ys[0]**2 - rates[1]*Ys[0]*Ys[1] + rates[2]*Ys[2]**2
     res[1] = rates[0]*Ys[0]**2 - rates[1]*Ys[1]*Ys[0]
@@ -22,7 +29,7 @@ def eq(Ys, rates, old, h):
     return res - h*(Ys-old)
 
 @numba.njit
-def inv_Jacobian(Ys, rates, old, h):
+def inv_Jacobian(Ys, rates, h):
     reac_1 = [-4*rates[0]*Ys[0]-rates[1]*Ys[1]-h,
               -rates[1]*Ys[0],
               2*rates[2]*Ys[2],
@@ -37,63 +44,77 @@ def inv_Jacobian(Ys, rates, old, h):
     reac_4 = [0,0,2*rates[2]*Ys[2], -h]
     return np.linalg.inv(np.array([reac_1, reac_2, reac_3, reac_4]))
 
-#@numba.njit
-def newton_raphson(oldY, invJ, fsol, args, maxsteps = 5, tol=1e-5):
+@numba.njit
+def newton_raphson(oldY, invJ, fsol, args, maxsteps = 3, tol=1e-2):
+    As = np.array([1,2,3,4])
+    prevY = oldY.copy()
+    rates, h = args
+    conv_flag = False
+    #while not conv_flag:
     for nr_step in range(maxsteps):
-        newY = oldY - np.dot(invJ(oldY, *args), fsol(oldY, *args))
-        diff = np.linalg.norm(fsol(newY, *args))
-        if diff<tol:
+        try:
+            newY = oldY - np.dot(invJ(oldY, rates, h), 
+                                 fsol(oldY, prevY, rates, h))
+        except:
             break
+        if newY[1]<1e-6:
+            newY[1]= 1e-6 # newY[0] * DtoH # rates[0] * newY[0]**2 / h
         oldY = newY
-    
-    #print(fsol(newY, *args))
-    sol = np.linalg.norm(fsol(newY, *args))
-    #print(diff)
 
-    return newY, sol
+        # crit = np.abs(1 - np.sum(newY))
+        # if crit < tol:
+        #     break
+        # conv_flag = True
+        # h = h*2
+        # print('timestep decreased')
+    return newY
 
 
-Hyd = 0.7
+Hyd = 0.91
 DtoH = 2.1e-5 # Geiss Gloeckler 98
 Deut = Hyd * DtoH
 He3toH = 1.5e-5 # Geiss Gloeckler 98
 He3 = Hyd * He3toH
-He = 0.28
+He = 0.089
 
-# Hyd = 1
-# DtoH = 2.1e-5 # Geiss Gloeckler 98
-# Deut = DtoH
-# He3toH = 1.5e-5 # Geiss Gloeckler 98
-# He3 = He3toH
-# He = 0.28/0.7
 year = 365*24*60*60 # [s]
-h = 1/(1e3*year) # 1/dT, 
-timesteps = int(5e3)
+hmax = 1/(1e4*year) # 1/dT, 
+h = hmax
+timesteps = int(1e6)
 Ys = np.zeros((timesteps, 4))
 sols = np.zeros((timesteps, 4))
+As = np.array([1,2,3,4])
 Ys[0] = [Hyd, Deut, He3, He]
-rates = np.array([1.25e-19,	1.9e-2, 1e-9])
+rates = np.array([7.9e-20,	1.01e-2, 2.22e-10])
 
+elapsed_time = 0
+max_time = 10e9*year
 for i in tqdm(range(1,timesteps)):
-    Ys[i], sols[i] = newton_raphson(Ys[i-1], inv_Jacobian, eq, args = (rates, Ys[i-1], h))
-    #print(f"({np.round((i+1)/len(Ys))}%): {Ys[i]}",end='\r')
-    
+    Ys[i] = newton_raphson(Ys[i-1], inv_Jacobian, eq,
+                                    args = (rates, h),)
+    elapsed_time += 1/h
+    if elapsed_time > max_time:
+        break
+    # h = np.min( [hmax, h/2] )
+print('Evo time', elapsed_time/(1e9*year), 'Gyrs')
+#%%
 labels = ["H", "D", "$^{3}$He", "$^{4}$He"]
-colors = ["k", "tab:red", "b", "green"]
+colors = ["k", "tab:red", "b", "darkorange"]
 linestyles = ["-","-","-","--"]
 plt.figure(tight_layout=True)
-for i,abundances in enumerate(Ys.T):
-    plt.plot(np.arange(timesteps), abundances, label = labels[i], ls=linestyles[i], color=colors[i], marker='')
 
-#plt.plot(np.arange(timesteps), Ys.sum(axis=1), 'k--')
+step = 1000
+try:
+    stop = np.where(Ys.T[0] == 0)[0][0]
+except:
+    stop = -1
+for i,abundances in enumerate(Ys.T):
+    plt.plot(np.arange(timesteps)[:stop:step]*1e-5, abundances[:stop:step], 
+             label = labels[i], ls=linestyles[i], color=colors[i], marker='')
 
 plt.grid()
 plt.yscale('log')
+plt.xscale('log')
 plt.ylabel('Abundance', fontsize = 14)
-plt.xlabel('time', fontsize = 14)
-#plt.ylim(1e-1,1)
-plt.legend(ncols = 1, loc='upper left', bbox_to_anchor = (1,1))
-
-fig, axs = plt.subplots(len(sols[0]),1, sharex=True)
-for i,sol in enumerate(sols.T):
-    axs[i].plot(np.arange(timesteps), sol, label = labels[i], ls=linestyles[i], color=colors[i], marker='')
+plt.xlabel('time [Gyrs]', fontsize = 14)
+plt.legend(ncols = 1)
